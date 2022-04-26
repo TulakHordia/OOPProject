67f98f7c1d357ff6efac36cb1e725547262e698d
TulakHordia <geshemsrain@gmail.com>
2022-04-26 13:36:43 +0300

    Update Manager.java

--- src/id319520425/Manager.java
+++ src/id319520425/Manager.java
@@ -1,10 +1,12 @@
 package id319520425;
 
+import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -14,6 +16,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,14 +26,14 @@ public class Manager {
 	ArrayList<Question> allQuestions = new ArrayList<Question>();
 	ArrayList<Question> manualExamArray = new ArrayList<Question>();
 	ArrayList<Question> autoExamArray = new ArrayList<Question>();
-	private int amountOfQuestions;
+	ArrayList<File> allExistingExams = new ArrayList<File>();
+	LinkedHashSet<File> set = new LinkedHashSet<File>();
 	private int size;
 	private int examNum;
 
 	Scanner input = new Scanner(System.in);
 	
 	public Manager() {
-		amountOfQuestions = 0;
 		size = 0;
 		examNum = 1;
 	}
@@ -43,28 +47,32 @@ public class Manager {
 	}
 	
 	public boolean addOpenQuestions(String question, String answer) {		
-		for (int i = 0; i < amountOfQuestions; i++) {
-			if (allQuestions.get(i).getQuestion().equals(question)) {
-				System.out.println("Question already exists.");
-				return false;
+		for (int i = 0; i < allQuestions.size(); i++) {
+			if (allQuestions.get(i) instanceof OpenQ) {
+				OpenQ oQ = (OpenQ) allQuestions.get(i);
+				if (oQ.getQuestion().equals(question)) {
+					System.out.println("Question already exists.");
+					return false;
+				}
 			}
 		}
 		allQuestions.add(new OpenQ(question, answer));
-		System.out.println("Created question #" + (amountOfQuestions + 1));
-		amountOfQuestions++;
+		System.out.println("Created question #" + (allQuestions.size()));
 		return true;
 	}
 
 	public boolean addAmericanQuestion(AmericanQ question) {
-		for (int i = 0; i < amountOfQuestions; i++) {
-			if (allQuestions.get(i).getQuestion().equals(question.getQuestion())) {
-				System.out.println("Question already exists.");
-				return false;
+		for (int i = 0; i < allQuestions.size(); i++) {
+			if (allQuestions.get(i) instanceof AmericanQ) {
+				AmericanQ aQ = (AmericanQ) allQuestions.get(i);
+				if (aQ.getQuestion().equals(question.getQuestion())) {
+					System.out.println("Question already exists.");
+					return false;
+				}
 			}
 		}
 		allQuestions.add(question);
-		System.out.println("Created question #" + (amountOfQuestions + 1));
-		amountOfQuestions++;
+		System.out.println("Created question #" + (allQuestions.size()));
 		return true;
 	}
 
@@ -166,6 +174,15 @@ public class Manager {
 			}
 		}
 	}
+	
+	public void printAmericanQuestionsOnly() {
+		System.out.println("-----American Questions-----");
+		for (int i = 0; i < allQuestions.size(); i++) {
+			if (allQuestions.get(i) instanceof AmericanQ) {
+				System.out.println((i+1)+") "+allQuestions.get(i).getQuestion());
+			}
+		}
+	}
 
 	public void printOpen() {
 		System.out.println("-----Open Questions-----");
@@ -191,8 +208,8 @@ public class Manager {
 	
 	public void write(String filename, ArrayList<Question> theExamArray) throws IOException {
 		
-		FileWriter fwQ = new FileWriter("exam_"+examNum+"_"+filename+"_questions.txt");
-		FileWriter fwA = new FileWriter("exam_"+examNum+"_"+filename+"_solution.txt");
+		FileWriter fwQ = new FileWriter("Exams/"+"exam_"+examNum+"_"+filename+"_questions.txt");
+		FileWriter fwA = new FileWriter("Exams/"+"exam_"+examNum+"_"+filename+"_solution.txt");
 		examNum++;
 
 		for (int i = 0; i < theExamArray.size(); i++) {
@@ -232,8 +249,8 @@ public class Manager {
 		boolean check = new File(fileName).exists();
 		
 		if (!check) {
-			FileWriter fwAll = new FileWriter(fileName);
-			System.out.println("Created a new " + fileName + " File.");
+			FileWriter fwAll = new FileWriter("Exams/"+fileName);
+			System.out.println("Created a new " + fileName + " File in Exams folder.");
 			
 			for (int i = 0; i < allQuestions.size(); i++) {
 				fwAll.write("Question number: " + (i+1) + "\n" + allQuestions.get(i)+"\n");
@@ -245,7 +262,7 @@ public class Manager {
 		else {
 			System.out.println("A 'questions.txt' file already exists.");
 			System.out.println("Added all ");
-			FileWriter fwAll = new FileWriter(fileName, true);
+			FileWriter fwAll = new FileWriter("Exams/"+fileName, true);
 
 			for (int i = 0; i < allQuestions.size(); i++) {
 				fwAll.write("Question number: " + (i+1) + "\n" + allQuestions.get(i)+"\n");
@@ -257,40 +274,76 @@ public class Manager {
 		System.out.println("-----Saved all questions & answers.-----");
 	}
 
-	public void readFile() throws FileNotFoundException {
-	
-		File f = new File("questions.txt");
-		Scanner reader = new Scanner(f);
-		
-		while (reader.hasNextLine()) {
-			String str = reader.nextLine();
-			System.out.println(str);
-		}
-		
-		reader.close();
-	}
 	
 	public void saveToBinaryFile() throws IOException, FileNotFoundException {
-		ObjectOutputStream outFile = new ObjectOutputStream(new FileOutputStream("questions.ser"));
+		ObjectOutputStream outFile = new ObjectOutputStream(new FileOutputStream("Exams/questions.ser"));
 		for (int i = 0; i < allQuestions.size(); i++) {
 			outFile.writeObject(allQuestions.get(i));
 		}
 		outFile.close();
-		System.out.println("Saved to: questions.ser");
+		System.out.println("Saved to: Exams/questions.ser");
 	}
 	
 	public void readFromBinaryFile(String fileName) throws IOException, FileNotFoundException, ClassNotFoundException {
-		try (ObjectInputStream inFile = new ObjectInputStream(new FileInputStream(fileName))){
+		try (ObjectInputStream inFile = new ObjectInputStream(new FileInputStream("Exams/"+fileName))){
 			while (true) {
 				allQuestions.add((Question) inFile.readObject());
 			}
 		} catch (EOFException e) {
-			System.out.println("Imported data.");
+			System.out.println("Imported data from " + fileName);
 		} catch (FileNotFoundException e) {
 			System.out.println("File " + fileName + " does not exist in the directory.");
 			e.getMessage();
 			e.printStackTrace();
 		} 
+	}
+	
+	public void autoImportOnLaunch() throws ClassNotFoundException, IOException {
+		try (ObjectInputStream inFile = new ObjectInputStream(new FileInputStream("Exams/questions.ser"))){
+			while (true) {
+				allQuestions.add((Question) inFile.readObject());
+			}
+		} catch (EOFException e) {
+			System.out.println("Automatically Imported data from questions.ser");
+		} 
+		Question quest = getQuestionById(allQuestions.size()-1);
+		quest.setQuestionNumber(allQuestions);
+	}
+	
+	public void showAllExistingExamsInDirectory() {
+		File[] files = new File("Exams/").listFiles();
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].isFile()) {
+				allExistingExams.add(files[i]);
+			}
+		}
+		removeDuplicates(allExistingExams);
+		Iterator itr = set.iterator();
+		int fNum = 1;
+		while (itr.hasNext()) {
+			System.out.println(fNum+") "+itr.next());
+			fNum++;
+		}
+	}
+	
+	public LinkedHashSet<File> removeDuplicates(ArrayList<File> allEArray) {
+		for (int i = 0; i < allEArray.size(); i++) {
+			set.add(allEArray.get(i));
+		}
+		return set;
+	}
+	
+	public void copyExistingExam(int copyChoice) throws IOException {
+		BufferedReader inputS = new BufferedReader(new FileReader(allExistingExams.get(copyChoice-1)));
+		FileWriter output = new FileWriter("Exams/"+"copy_of_"+allExistingExams.get(copyChoice-1).getName());
+		String count;
+		while ( (count = inputS.readLine()) != null ) {
+			output.write(count);
+		}
+		System.out.println("Created a copy of: " + allExistingExams.get(copyChoice-1).getName());
+		output.flush();
+		output.close();
+		inputS.close();
 	}
 
 	public void sortByAnswerLength(ArrayList<Question> manualExamArrayInput) {
@@ -309,10 +362,11 @@ public class Manager {
 				System.out.println(theExamArray.get(i));
 			}
 		}
-		System.out.println("Exam created, contains: " + theExamArray.size() + " questions");
+		System.out.println("Exam created on the: " + getDateTime() + ", contains: " + theExamArray.size() + " questions.");
+		autoExamArray.clear();
 	}
 
-	public void sortAndPrintManualExamArray() {
+	public void sortAndPrintManualExamArray() throws IOException {
 		sortByAnswerLength(manualExamArray);
 		for (int i = 0; i < manualExamArray.size(); i++) {
 			if (manualExamArray.get(i) != null) {
@@ -320,13 +374,16 @@ public class Manager {
 			}
 		}
 		
+		write(getDateTime(), manualExamArray);
+		
 		int counter = 0;
 		for (int i = 0; i < manualExamArray.size(); i++) {
 			if (manualExamArray.get(i) != null) {
 				counter++;
 			}
 		}
-		System.out.println("Exam created, contains: " + counter + " questions");
+		System.out.println("Exam created on the: " + getDateTime() + ", contains: " + counter + " questions.");
+		manualExamArray.clear();
 	}
 
 	public boolean checkQuestionIsInArray(ArrayList<Question> autoExamArray2, Question quest) {
@@ -425,7 +482,6 @@ public class Manager {
 				if (aQ.getAnswers(i).getAnswer() != null) {
 					System.out.println("[" + (i + 1) + "] " + aQ.getAnswers(i));
 				}
-//			System.out.println("[" + (i + 1) + "] " + aQ.getAnswers(i));
 			}
 		}
 	}
@@ -456,7 +512,6 @@ public class Manager {
 				return true;
 			}
 		}
-		System.out.println("test 2:"+manualExamArray.get(0) + " " + manualExamArray.get(1));
 		return false;
 	}
 
@@ -529,7 +584,13 @@ public class Manager {
 	
 	private final static String getDateTime()  
 	{  
-	    DateFormat df = new SimpleDateFormat("yyyy-MM-dd");  
+	    DateFormat df = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss");  
 	    return df.format(new Date());  
+	}
+
+	public void sortAllQuestions() {
+		QuestionComparator qC = new QuestionComparator();
+		allQuestions.sort(qC);
+		System.out.println("Sorted all questions array.");
 	}  
 }
